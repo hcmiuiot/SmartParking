9eaa6376f01ea6e47271ca2f62800d43ccd09924
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2018-11-05 23:26:17 +0700

    Big update

--- src/main/java/main/TrackingConfigController.java
+++ src/main/java/main/TrackingConfigController.java
@@ -1,16 +1,14 @@
 package main;
 
-import java.net.URL;
-import java.security.cert.TrustAnchor;
-import java.util.ResourceBundle;
-
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXSlider;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class TrackingConfigController implements Initializable {
 
