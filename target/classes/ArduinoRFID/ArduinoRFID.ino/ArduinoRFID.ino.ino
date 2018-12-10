0d22b937765106242099fbd508a417b00821c61f
Nguyễn Thanh Xuân Tùng <xuantung.1998@gmail.com>
2018-12-11 00:14:15 +0700

    Now can read serial

--- target/classes/ArduinoRFID/ArduinoRFID.ino/ArduinoRFID.ino.ino
+++ target/classes/ArduinoRFID/ArduinoRFID.ino/ArduinoRFID.ino.ino
@@ -22,10 +22,10 @@ void setup() {
 
   for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
     mfrc522[reader].PCD_Init(ssPins[reader], RST_PIN); // Init each MFRC522 card
-    Serial.print(F("Reader "));
-    Serial.print(reader);
-    Serial.print(F(": "));
-    mfrc522[reader].PCD_DumpVersionToSerial();
+//    Serial.print(F("Reader "));
+//    Serial.print(reader);
+//    Serial.print(F(": "));
+//    mfrc522[reader].PCD_DumpVersionToSerial();
   }
 }
 
